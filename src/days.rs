abb6779885e4fe813ab6d233836bc31e8081084e
Nafi Robayat <nafi@live.com>
2025-08-03 21:55:21 +1000

    Initalised Repository

---
+++ src/days.rs
@@ -0,0 +1,126 @@
+#![allow(dead_code)]
+
+use std::collections::HashMap;
+use std::fs;
+use std::iter::zip;
+
+pub fn day1() {
+    day1_p1();
+    day1_p2();
+}
+
+fn day1_p1() {
+    let file_name: &str = "data/day1.txt";
+
+    let contents: String = fs::read_to_string(file_name).expect("Found file");
+    let mut list1: Vec<i32> = Vec::new();
+    let mut list2: Vec<i32> = Vec::new();
+
+    let lines = contents.split('\n');
+    for line in lines {
+        let nums: Vec<&str> = line.split("   ").collect();
+        if nums.len() < 2 {
+            break;
+        }
+        let num1: i32 = nums[0].parse::<i32>().unwrap();
+        let num2: i32 = nums[1].parse::<i32>().unwrap();
+        list1.push(num1);
+        list2.push(num2);
+    }
+
+    let mut total: i32 = 0;
+    list1.sort();
+    list2.sort();
+
+    for (num1, num2) in zip(&list1, &list2) {
+        total += (num1 - num2).abs()
+    }
+    println!("Day 1 Part 1 Answer: {}", total)
+}
+
+fn day1_p2() {
+    let file_name = "data/day1.txt";
+    let contents: String = fs::read_to_string(file_name).expect("Found file");
+    let mut list1: Vec<i32> = Vec::new();
+    let mut list2: Vec<i32> = Vec::new();
+    let lines = contents.split('\n');
+    for line in lines {
+        let nums: Vec<&str> = line.split("   ").collect();
+        if nums.len() < 2 {
+            break;
+        }
+        let num1: i32 = nums[0].parse::<i32>().unwrap();
+        let num2: i32 = nums[1].parse::<i32>().unwrap();
+        list1.push(num1);
+        list2.push(num2);
+    }
+
+    let mut map: HashMap<&i32, i32> = HashMap::new();
+    for num in list2.iter() {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+    }
+
+    let mut total = 0;
+    for num in list1.iter() {
+        let multiplier = match map.get(num) {
+            Some(n) => *n,
+            None => 0,
+        };
+        total += num * multiplier;
+    }
+    println!("Day 1 Part 2 Answer: {}", total);
+}
+pub fn day2() {
+    day2_p1();
+}
+
+fn day2_p1() {
+    let file_name = "data/day2.txt";
+    let contents: String = fs::read_to_string(file_name).expect("Found file");
+
+    let lines = contents.split('\n');
+
+    let mut num_safe: i32 = 0;
+
+    for line in lines {
+        let arr: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+        if arr.len() <= 1 {
+            break;
+        }
+
+        let is_positive = arr[0] < arr[1];
+        let mut is_safe = true;
+
+        for i in 1..arr.len() {
+            let diff = arr[i] - arr[i - 1];
+            if diff == 0 {
+                is_safe = false;
+                break;
+            }
+            if diff < 0 && is_positive {
+                is_safe = false;
+                break;
+            }
+
+            if diff > 0 && !is_positive {
+                is_safe = false;
+                break;
+            }
+
+            if diff.abs() != 1 && diff.abs() != 2 && diff.abs() != 3 {
+                is_safe = false;
+                break;
+            }
+        }
+
+        if is_safe {
+            num_safe += 1;
+        }
+    }
+
+    println!("Day 2 Part 1 Answer: {}", num_safe);
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod days;
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Not enough arguments given! Format: aoc2024 <days>");
+        process::exit(1);
+    }
+
+    let day_str: &str = &args[1];
+
+    let day = match day_str.parse::<i32>() {
+        Ok(d) => d,
+        Err(_) => {
+            println!("Error bad day given");
+            process::exit(1);
+        }
+    };
+
+    match day {
+        1 => days::day1(),
+        2 => days::day2(),
+        _ => {
+            println!("{day} not found");
+            process::exit(1);
+        }
+    }
+}
